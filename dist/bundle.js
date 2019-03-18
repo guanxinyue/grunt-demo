b7668e65daac14351271523a4a2b9a27e4e356f8
guanxinyue <778799700@qq.com>
2019-03-18 20:19:05 +0800

    concat

---
+++ dist/bundle.js
@@ -0,0 +1,41 @@
+$(function() {
+  var $width   = $('#rectangle-width'),
+      $height  = $('#rectangle-height'),
+      $calc    = $('#rectangle-calc'),
+      blnValid = false;
+
+  $calc.click(function() {
+    if(!blnValid) return;
+
+    var width  = Number($width.val()),
+        height = Number($height.val()),
+        p      = roundFractional(width * 2 + height * 2, 2),
+        a      = roundFractional(width * height, 2);
+
+    $('#rectangle-perimeter').val(p);
+    $('#rectangle-area').val(a);
+  });
+
+  $width.focusout(function() {
+    blnValid = validate('#rectangle-width');
+  });
+  
+  $height.focusout(function() {
+    blnValid = validate('#rectangle-height');
+  });
+
+function Rectangle(width, height) {
+    var w = Number(width),
+              h = Number(height);
+
+    this.area = function() {
+          return w * h;
+            
+    };
+
+    this.perimeter = function() {
+          return 2 * (w + h);
+            
+    };
+
+}
