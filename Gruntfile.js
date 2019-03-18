5625f17c5f58b2c41433929ac18f6740e52d8ecd
guanxinyue <778799700@qq.com>
2019-03-18 21:16:54 +0800

    sprite

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sprite: {
+      all: {
+        src: './images/*.png',
+        dest: './images/all.png',
+        destCss: 'dist/sprites.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-spritesmith');
+
+  grunt.registerTask('default', ['sprite']);
+};
