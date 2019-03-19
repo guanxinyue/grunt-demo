fdac4a03bbd58d23d3076d15bab0477b55f580a0
guanxinyue <778799700@qq.com>
2019-03-19 12:20:10 +0800

    entropy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,34 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    sprite: {
-      all: {
-          src: './images/*.png',
-          dest: './images/all.png',
-          destCss: 'dist/sprites.css'                                            
+    mochacli: {
+      options: {
+        reporter: 'spec',
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    mocha_istanbul: {
+      coverage: {
+        src: 'test'
       }
-          
+    },
+    istanbul_check_coverage: {
+      default: {
+        options: {
+          coverageFolder: 'coverage*',
+          check: {
+            lines: 90,
+            statements: 90
+          }
+        }
+      }
     }
-      
   });
 
-    grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-    grunt.registerTask('default', ['sprite']);
-
+  grunt.registerTask('default', ['mochacli']);
+  grunt.registerTask('cover', ['mocha_istanbul']);
+  grunt.registerTask('cover-check', ['istanbul_check_coverage']);
 };
